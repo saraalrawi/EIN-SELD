32f68c9f75dc7815fdab4ba01cf3b4b8d2d656f7
Sara AlRawi <sarah.jamal86@gmail.com>
2020-12-17 22:11:31 +0100

    orthogonal loss

--- seld/methods/utils/loss_utilities.py
+++ seld/methods/utils/loss_utilities.py
@@ -35,3 +35,24 @@ class BCEWithLogitsLoss:
         else:
             return self.loss(pred, target).mean(dim=tuple(range(2, pred.ndim)))
 
+class OrthogonalLoss(nn.Module):
+    def __init__(self):
+        super(OrthogonalLoss, self).__init__()
+    def forward(self, input1, input2):
+        diff_loss = 0
+        return diff_loss
+
+    def calculate_orthogonal_loss(mat_1, mat_2, stride=None):
+        """
+        this function finds the orthogonality distance between the layers in the model.
+        Params:
+            mat_1: layer 1
+            mat_2: layer 2
+        Returns:
+            orth_dist: orthogonality distance
+        """
+        mat_1 = mat_1.reshape((mat_1.shape[0], -1))
+        if mat_1.shape[0] < mat_1.shape[1]:
+            mat = mat_1.permute(1, 0)
+        orth_dist = torch.norm(torch.t(mat_1) @ mat_2 - torch.eye(mat_1.shape[1]).cuda())
+        return orth_dist
