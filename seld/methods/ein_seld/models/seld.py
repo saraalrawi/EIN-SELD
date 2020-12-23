f656e34acd4a1b8b124dfd207207651c63cabef4
Sara AlRawi <sarah.jamal86@gmail.com>
2020-12-23 10:01:25 +0100

    consider 7 channels for sed instead of 4

--- seld/methods/ein_seld/models/seld.py
+++ seld/methods/ein_seld/models/seld.py
@@ -17,7 +17,7 @@ class EINV2(nn.Module):
 
         self.downsample_ratio = 2 ** 2
         self.sed_conv_block1 = nn.Sequential(
-            DoubleConv(in_channels=4, out_channels=64),
+            DoubleConv(in_channels=self.in_channels, out_channels=64),
             nn.AvgPool2d(kernel_size=(2, 2)),
         )
         self.sed_conv_block2 = nn.Sequential(
@@ -87,7 +87,7 @@ class EINV2(nn.Module):
         """
         x: waveform, (batch_size, num_channels, data_length)
         """
-        x_sed = x[:, :4]
+        x_sed = x[:, :]
         x_doa = x
 
         # cnn
