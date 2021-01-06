5f6e847d43b050fa24283967c6b9fa9de150ab22
Sara AlRawi <sarah.jamal86@gmail.com>
2021-01-06 18:02:19 +0100

    stronger weight orthogonality

--- seld/methods/ein_seld/losses.py
+++ seld/methods/ein_seld/losses.py
@@ -7,9 +7,10 @@ from methods.utils.loss_utilities import BCEWithLogitsLoss, MSELoss
 
 
 class Losses:
-    def __init__(self, cfg):
+    def __init__(self, cfg, args):
         
         self.cfg = cfg
+        self.args = args
         self.beta = cfg['training']['loss_beta']
         self.decay = cfg['training']['orthogonal_decay']
         self.losses = [BCEWithLogitsLoss(reduction='mean'), MSELoss(reduction='mean')]
@@ -25,9 +26,10 @@ class Losses:
             loss_sed = self.losses[0].calculate_loss(pred['sed'], updated_target['sed'])
             loss_doa = self.losses[1].calculate_loss(pred['doa'], updated_target['doa'])
         elif self.cfg['training']['PIT_type'] == 'tPIT':
-            loss_sed, loss_doa, updated_target = self.tPIT(pred, target)
-            #if self.cfg['training']['constraints'] == 'orthogonal':
-            #    loss_orthogonal = self.orthogonal_distance(model)
+            loss_sed, loss_doa, updated_target, loss_doa_smoothness = self.tPIT(pred, target)
+            if self.cfg['training']['weight_constraints'] == 'orthogonal':
+                loss_orthogonal = self.orthogonal_distance(model)
+            '''
             if self.cfg['training']['layer_constraints'] == 'orthogonal':
                 loss_orthogonal_layer = self.orthogonal_layer_distance(model.module.sed_conv_block1[0].double_conv[0].weight,model.module.doa_conv_block1[0].double_conv[0].weight) \
                                   + self.orthogonal_layer_distance(model.module.sed_conv_block1[0].double_conv[3].weight,model.module.doa_conv_block1[0].double_conv[3].weight) \
@@ -37,9 +39,66 @@ class Losses:
                                   + self.orthogonal_layer_distance(model.module.sed_conv_block3[0].double_conv[3].weight,model.module.doa_conv_block3[0].double_conv[3].weight) \
                                   + self.orthogonal_layer_distance(model.module.sed_conv_block4[0].double_conv[0].weight,model.module.doa_conv_block4[0].double_conv[0].weight) \
                                   + self.orthogonal_layer_distance(model.module.sed_conv_block4[0].double_conv[3].weight,model.module.doa_conv_block4[0].double_conv[3].weight)
+            '''
+            # stronger weight orthogonality
+            if self.cfg['training']['weight_constraints_1'] == 'orthogonal':
+                loss_orthogonal = self.orth_dist(model.module.sed_conv_block1[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block1[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block2[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block2[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block3[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block3[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block4[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.sed_conv_block4[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.doa_conv_block1[0].double_conv[0].weight)  \
+                                  + self.orth_dist(model.module.doa_conv_block1[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.doa_conv_block2[0].double_conv[0].weight)  \
+                                  + self.orth_dist(model.module.doa_conv_block2[0].double_conv[3].weight)  \
+                                  + self.orth_dist(model.module.doa_conv_block3[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.doa_conv_block3[0].double_conv[3].weight) \
+                                  + self.orth_dist(model.module.doa_conv_block4[0].double_conv[0].weight) \
+                                  + self.orth_dist(model.module.doa_conv_block4[0].double_conv[3].weight)
 
-        '''
-        if self.cfg['training']['constraints'] == 'orthogonal':
+                loss_orthogonal += self.deconv_orth_dist(model.module.sed_conv_block1[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.sed_conv_block1[0].double_conv[3].weight) + self.deconv_orth_dist(
+                    model.module.sed_conv_block2[0].double_conv[0].weight)
+                loss_orthogonal += self.deconv_orth_dist(model.module.sed_conv_block2[0].double_conv[3].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.sed_conv_block3[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.sed_conv_block3[0].double_conv[3].weight, stride=1)
+                loss_orthogonal += self.deconv_orth_dist(model.module.sed_conv_block4[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.sed_conv_block4[0].double_conv[3].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block1[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block1[0].double_conv[3].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block2[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block2[0].double_conv[3].weight, stride=1)
+                loss_orthogonal += self.deconv_orth_dist(model.module.doa_conv_block3[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block3[0].double_conv[3].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block4[0].double_conv[0].weight, stride=1) + self.deconv_orth_dist(
+                    model.module.doa_conv_block4[0].double_conv[3].weight, stride=1)
+
+            if self.cfg['training']['layer_constraints_1'] == 'orthogonal':
+                loss_orthogonal = self.orth_dist_layer(model.module.sed_conv_block1[0].double_conv[0].weight,model.module.doa_conv_block1[0].double_conv[0].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block1[0].double_conv[3].weight, model.module.doa_conv_block1[0].double_conv[3].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block2[0].double_conv[0].weight,model.module.doa_conv_block2[0].double_conv[0].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block2[0].double_conv[3].weight,model.module.doa_conv_block2[0].double_conv[3].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block3[0].double_conv[0].weight,model.module.doa_conv_block3[0].double_conv[0].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block3[0].double_conv[3].weight,model.module.doa_conv_block3[0].double_conv[3].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block4[0].double_conv[0].weight, model.module.doa_conv_block4[0].double_conv[0].weight) \
+                                  + self.orth_dist_layer(model.module.sed_conv_block4[0].double_conv[3].weight,model.module.doa_conv_block4[0].double_conv[3].weight)
+
+                loss_orthogonal += self.deconv_orth_dist_layer(model.module.sed_conv_block1[0].double_conv[0].weight,model.module.doa_conv_block1[0].double_conv[0].weight ,
+                                                         stride=1) + self.deconv_orth_dist_layer(
+                    model.module.sed_conv_block1[0].double_conv[3].weight,model.module.doa_conv_block1[0].double_conv[3].weight) + self.deconv_orth_dist_layer(
+                    model.module.sed_conv_block2[0].double_conv[0].weight, model.module.doa_conv_block2[0].double_conv[0].weight)
+                loss_orthogonal += self.deconv_orth_dist_layer(model.module.sed_conv_block2[0].double_conv[3].weight,model.module.doa_conv_block2[0].double_conv[3].weight ,
+                                                         stride=1) + self.deconv_orth_dist_layer(
+                    model.module.sed_conv_block3[0].double_conv[0].weight, model.module.doa_conv_block3[0].double_conv[0].weight,  stride=1) + self.deconv_orth_dist_layer(
+                    model.module.sed_conv_block3[0].double_conv[3].weight,model.module.doa_conv_block3[0].double_conv[3].weight ,stride=1)
+                loss_orthogonal += self.deconv_orth_dist_layer(model.module.sed_conv_block4[0].double_conv[0].weight,model.module.doa_conv_block4[0].double_conv[0].weight ,
+                                                         stride=1) + self.deconv_orth_dist_layer(
+                    model.module.sed_conv_block4[0].double_conv[3].weight,model.module.doa_conv_block4[0].double_conv[3].weight, stride=1)
+
+        if self.cfg['training']['weight_constraints'] == 'orthogonal':
             orthogonal_constraint_loss = self.adjust_ortho_decay_rate(epoch_it + 1) * loss_orthogonal
             loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + orthogonal_constraint_loss
 
@@ -47,21 +106,57 @@ class Losses:
                 'all': loss_all,
                 'sed': loss_sed,
                 'doa': loss_doa,
-                'orthogonal': orthogonal_constraint_loss,
+                'loss_weight_orthogonal': orthogonal_constraint_loss,
                 'updated_target': updated_target
                 }
+            ''' 
+            if self.cfg['training']['layer_constraints'] == 'orthogonal':
+                orthogonal_layer_constraint_loss = self.adjust_ortho_decay_rate(epoch_it + 1) * loss_orthogonal_layer
+                loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + orthogonal_layer_constraint_loss
+
+                losses_dict = {
+                    'all': loss_all,
+                    'sed': loss_sed,
+                    'doa': loss_doa,
+                    'loss_layer_orthogonal': orthogonal_layer_constraint_loss,
+                    'updated_target': updated_target
+                    }
         '''
-        if self.cfg['training']['layer_constraints'] == 'orthogonal':
-            orthogonal_layer_constraint_loss = self.adjust_ortho_decay_rate(epoch_it + 1) * loss_orthogonal_layer
-            loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + orthogonal_layer_constraint_loss
+        elif self.cfg['training']['layer_constraints_1'] == 'orthogonal':
+                orthogonal_constraint_loss = self.args.r * loss_orthogonal
+                loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + orthogonal_constraint_loss
+
+                losses_dict = {
+                    'all': loss_all,
+                    'sed': loss_sed,
+                    'doa': loss_doa,
+                    'loss_layer_orthogonal_1': orthogonal_constraint_loss,
+                    'updated_target': updated_target
+                    }
+
+        elif self.cfg['training']['weight_constraints_1'] == 'orthogonal':
+            # no weight decay self.cfg['training']['r']
+            orthogonal_constraint_loss =  self.args.r * loss_orthogonal
+            loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + orthogonal_constraint_loss
 
             losses_dict = {
                 'all': loss_all,
                 'sed': loss_sed,
                 'doa': loss_doa,
-                'loss_layer_orthogonal': orthogonal_layer_constraint_loss,
+                'loss_weight_orthogonal_1': orthogonal_constraint_loss,
                 'updated_target': updated_target
-            }
+                }
+
+        elif self.cfg['training']['smoothness_loss']:
+            smoothness_weight = 1e-3
+            loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa + smoothness_weight * (loss_doa_smoothness)
+            losses_dict = {
+                'all': loss_all,
+                'sed': loss_sed,
+                'doa': loss_doa,
+                'updated_target': updated_target,
+                'loss_doa_smoothness': loss_doa_smoothness
+                }
         else:
             loss_all = self.beta * loss_sed + (1 - self.beta) * loss_doa
             losses_dict = {
@@ -108,6 +203,27 @@ class Losses:
 
         loss_sed = (loss_sed1 * (loss1 <= loss2) + loss_sed2 * (loss1 > loss2)).mean()
         loss_doa = (loss_doa1 * (loss1 <= loss2) + loss_doa2 * (loss1 > loss2)).mean()
+        loss_doa_smoothness = 0
+
+        if self.cfg['training']['smoothness_loss']:
+            smoothness_threshold = 1.0
+            doa_array = pred['doa']
+
+            # 1st derivative
+            d_doa_array_dt = doa_array[:, 1:, :, :] - doa_array[:, :-1, :, :]
+
+            # 2nd derivative
+            d2_doa_array_dt2 = d_doa_array_dt[:, 1:, :, :] - d_doa_array_dt[:, :-1, :, :]
+
+            # ignore non-significant non-smoothness
+            d2_doa_array_dt2 = torch.where(d2_doa_array_dt2 < smoothness_threshold,
+                                           torch.zeros_like(d2_doa_array_dt2), d2_doa_array_dt2)
+
+            # actual loss is MSE of d2_doa_array_dt2
+            loss_doa_smoothness = (d2_doa_array_dt2 ** 2).mean()
+            #loss_doa = loss_doa_smoothness * loss_doa
+
+
         updated_target_sed = target['sed'].clone() * (loss1[:, :, None, None] <= loss2[:, :, None, None]) + \
             target_flipped['sed'].clone() * (loss1[:, :, None, None] > loss2[:, :, None, None])
         updated_target_doa = target['doa'].clone() * (loss1[:, :, None, None] <= loss2[:, :, None, None]) + \
@@ -116,7 +232,7 @@ class Losses:
             'sed': updated_target_sed,
             'doa': updated_target_doa
         }
-        return loss_sed, loss_doa, updated_target
+        return loss_sed, loss_doa, updated_target, loss_doa_smoothness
 
     def orthogonal_distance(self, model):
         l2_reg = None
@@ -183,3 +299,72 @@ class Losses:
             o_d = 1e-3 * o_d
 
         return o_d
+
+    # paper: https://arxiv.org/abs/1911.12207
+    # For Kernel orthogonality
+    def deconv_orth_dist(self,kernel, stride=2, padding=1):
+        [o_c, i_c, w, h] = kernel.shape
+        output = torch.conv2d(kernel, kernel, stride=stride, padding=padding)
+        target = torch.zeros((o_c, o_c, output.shape[-2], output.shape[-1])).cuda()
+        ct = int(np.floor(output.shape[-1] / 2))
+        target[:, :, ct, ct] = torch.eye(o_c).cuda()
+        return torch.norm(output - target)
+
+    '''
+    def orth_dist(self,matr, stride=None):
+        mat = matr.reshape((matr.shape[0], -1))
+        if matr.shape[0] < matr.shape[1]:
+            matr = matr.permute(1, 0)
+        return torch.norm(torch.t(matr) @ mat - torch.eye(matr.shape[1]).cuda())
+    '''
+    def orth_dist(self, W, stride=None):
+        cols = W[0].numel()
+        rows = W.shape[0]
+        w1 = W.view(-1, cols)
+        wt = torch.transpose(w1, 0, 1)
+        m = torch.matmul(wt, w1)
+        ident = Variable(torch.eye(cols, cols))
+        ident = ident.cuda()
+
+        w_tmp = (m - ident)
+        height = w_tmp.size(0)
+        u = normalize(w_tmp.new_empty(height).normal_(0, 1), dim=0, eps=1e-12)
+        v = normalize(torch.matmul(w_tmp.t(), u), dim=0, eps=1e-12)
+        u = normalize(torch.matmul(w_tmp, v), dim=0, eps=1e-12)
+        sigma = torch.dot(u, torch.matmul(w_tmp, v))
+
+
+        l2_reg = (sigma) ** 2
+
+        return l2_reg
+
+    # paper: https://arxiv.org/abs/1911.12207
+    # For layers orthogonality
+    def orth_dist_layer(self, W_1, W_2 ,stride=None):
+        cols = W_1[0].numel()
+        rows = W_1.shape[0]
+        w1 = W_2.view(-1, cols)
+        wt = torch.transpose(w1, 0, 1)
+        m = torch.matmul(wt, w1)
+        ident = Variable(torch.eye(cols, cols))
+        ident = ident.cuda()
+
+        w_tmp = (m - ident)
+        height = w_tmp.size(0)
+        u = normalize(w_tmp.new_empty(height).normal_(0, 1), dim=0, eps=1e-12)
+        v = normalize(torch.matmul(w_tmp.t(), u), dim=0, eps=1e-12)
+        u = normalize(torch.matmul(w_tmp, v), dim=0, eps=1e-12)
+        sigma = torch.dot(u, torch.matmul(w_tmp, v))
+
+
+        l2_reg = (sigma) ** 2
+
+        return l2_reg
+
+    def deconv_orth_dist_layer(self,kernel,kernel_, stride=2, padding=1):
+        [o_c, i_c, w, h] = kernel.shape
+        output = torch.conv2d(kernel, kernel_, stride=stride, padding=padding)
+        target = torch.zeros((o_c, o_c, output.shape[-2], output.shape[-1])).cuda()
+        ct = int(np.floor(output.shape[-1] / 2))
+        target[:, :, ct, ct] = torch.eye(o_c).cuda()
+        return torch.norm(output - target)
