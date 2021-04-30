4ff2212fba9762f3fb390e2a6bf92da4ebeff275
Sara AlRawi <sarah.jamal86@gmail.com>
2021-04-30 11:15:43 +0200

    config files to be run by Jannik commented out

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,6 +2,20 @@
 
 set -e
 
-CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld.yaml'
 
-python3 seld/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1) --num_workers=8
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_training_att_0.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_0.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_5.yaml'
+CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_15.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_20.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_25.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_30.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_35.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_40.yaml'
+
+#for i in 1e-3 1e-2 1e-1
+#do
+  #echo " Decay value: $i"
+python3 -W ignore seld/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1)   --num_workers=8
+#done
