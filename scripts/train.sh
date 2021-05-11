01281ace5f80f725bb0a39434e9b8d20cb6d289f
Sara AlRawi <sarah.jamal86@gmail.com>
2021-05-11 17:31:34 +0200

    configs for Jannik

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 set -e
-
+#########################################################################################################################
+# Jannik, Just remove the comment ;-)
+#CONFIG_FILE= '/home/zuern/EIN-SELD/configs/ein_seld/seld_baseline_mic.yaml'
+#CONFIG_FILE= '/home/zuern/EIN-SELD/configs/ein_seld/seld_baseline_se_mic.yaml'
+#CONFIG_FILE= '/home/zuern/EIN-SELD/configs/ein_seld/seld_attention_mic.yaml'
+#########################################################################################################################
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld.yaml'
-
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_attention_foa.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_training_att_0.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_0.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_5.yaml'
-CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_15.yaml'
+#CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_15.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_20.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_25.yaml'
 #CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_30.yaml'
@@ -17,5 +22,6 @@ CONFIG_FILE='/home/alrawis/EIN-SELD/configs/ein_seld/seld_noisy_validation_att_1
 #for i in 1e-3 1e-2 1e-1
 #do
   #echo " Decay value: $i"
+echo " Running with config: $CONFIG_FILE"
 python3 -W ignore seld/main.py -c $CONFIG_FILE train --seed=$(shuf -i 0-10000 -n 1)   --num_workers=8
 #done
